bdafb04f6a9aa52eaefc5cbdc14304a45c1c8d0b
Jose Alves <msft38@mutantes.LABS-IC>
2022-09-10 12:32:23 -0300

    Implemented Class Jewel

--- JewelCollector/Program.cs
+++ JewelCollector/Program.cs
@@ -1,1 +1,49 @@
-﻿
+﻿namespace JewelCollector {
+    class Jewel{
+        public string Color { get; set; }
+        public int Point { get; set; }
+
+        public Jewel(string color){
+
+            Color = color;
+            Point = color switch  {
+                "red" => 100,
+                "blue" => 200,
+                "green" => 300,
+                _ => 0
+            };
+        }
+    }
+
+    abstract class ItemMap{
+
+    }
+
+    abstract class Obstacle : ItemMap
+    {
+
+    }
+
+    class Water : Obstacle
+    {
+
+    }
+
+    class Red : Jewel {
+
+        public Red(string s): base(s)
+        {
+            
+        }
+
+    }
+
+    class Blue : Jewel {
+
+    class Map {
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+        private ItemMap[,] Matrix = new ItemMap[10,10];
+    }
+}
